50e72acf23086d39588efe88c2b4a0c3273059b0
Zhiwei <chihwei.public@outlook.com>
2021-04-27 01:50:41 -0700

    Format file

--- client/send_message.py
+++ client/send_message.py
@@ -2,16 +2,17 @@ import psutil, json, time
 from datetime import datetime
 from clearblade.ClearBladeCore import System
 
+
 def get_pc_state():
     results = {}
     results["time"] = datetime.utcnow().isoformat()
     results["cpu_usage"] = psutil.cpu_percent(10)
     return json.dumps(results)
 
+
 # System credentials
 SYSTEM_KEY = "<your_system_key>"
 SYSTEM_SECRET = "<your_system_secrete>"
-
 
 
 pc_state_system = System(SYSTEM_KEY, SYSTEM_SECRET)
@@ -24,4 +25,3 @@ while True:
     mqtt.publish("pc_state", pc_state)
     mqtt.disconnect()
     time.sleep(900)
-
